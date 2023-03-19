3f2488778105ba6a0ae78ca4ad941dd3b7b19fcf
Pingbo Yang <86988793+PingboYang@users.noreply.github.com>
2023-03-19 00:06:36 -0400

    add code to send back data to Main

--- app/src/main/java/com/bignerdranch/android/lunchtoactivity2/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/lunchtoactivity2/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.lunchtoactivity2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private EditText mEditTextContent;
     private Button mButtonLaunchSecondActivity;
+    private static final int REQUEST_CODE_SECONDACTIVITY=0;
 
 
     @Override
@@ -30,10 +32,21 @@ public class MainActivity extends AppCompatActivity {
                 //get text content
                 String s=mEditTextContent.getText().toString();
                 //use Intent object to describe the new activity
-                Intent intent=new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+                //Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent=SecondActivity.newIntent(MainActivity.this,s);
+                startActivityForResult(intent,REQUEST_CODE_SECONDACTIVITY );
 
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode,resultCode,intent);
+        if(requestCode!= Activity.RESULT_OK) return;
+        if (requestCode==REQUEST_CODE_SECONDACTIVITY){
+            if(intent==null)
+                return;
+            String data=SecondActivity.getStringToMain(intent);
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/lunchtoactivity2/SecondActivity.java
+++ app/src/main/java/com/bignerdranch/android/lunchtoactivity2/SecondActivity.java
@@ -2,13 +2,54 @@ package com.bignerdranch.android.lunchtoactivity2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String EXTRA_EDITTEXT_CONTENT="com.bignerdranch.android.lunchtoactivity2.CONTENT";
+    private TextView mTextViewContent;
+    private Button mButtonGoBack;
+    private EditText mEditTextNumber;
+    private static final String EXTRA_SECOND_TO_MAIN="com.bignerdranch.android.lunchtoactivity2.NUMBER";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        //the getIntent() method is available to retrieve the Extra
+        String s=getIntent().getStringExtra(EXTRA_EDITTEXT_CONTENT);
+        mTextViewContent=(TextView) findViewById(R.id.textFromMainActivity);
+        mTextViewContent.setText(s);
+
+        mButtonGoBack=(Button) findViewById(R.id.buttonGoBack);
+        mEditTextNumber=(EditText) findViewById(R.id.numberToMain);
+        mButtonGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s=mEditTextNumber.getText().toString();
+                Intent intent=new Intent();
+                intent.putExtra(EXTRA_EDITTEXT_CONTENT,s);
+                //send the data back
+                setResult(RESULT_OK, intent);
+                finish();
+
+            }
+        });
+
+    }
+    //create a method for mainActivity to instantiate an Intent object
+    public static Intent newIntent(Context packageContext, String content){
+        Intent intent=new Intent(packageContext, SecondActivity.class);
+        intent.putExtra(EXTRA_SECOND_TO_MAIN,content);
+        return intent;
+    }
+    public static String getStringToMain(Intent result){
+        return result.getStringExtra(EXTRA_SECOND_TO_MAIN);
     }
 }
